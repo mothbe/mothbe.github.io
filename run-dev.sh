92baad3a167f9c354312374017a6f923737a484d
Marcin Witowski <mars3n@protonmail.com>
2026-09-13 13:27:37 +0200

    change theme

--- run-dev.sh
+++ run-dev.sh
@@ -1,1 +1,3 @@
-docker run --rm -p 1313:1313 -v ${PWD}:/src hugomods/hugo:0.118.2 hugo server --bind 0.0.0.0 -p 1313
+docker run --rm -p 1313:1313 -v ${PWD}:/src \
+  docker.io/hugomods/hugo@sha256:cf1a2009a1edf807d0880af344c4dd13b40b308c340501128fb54e5aab17b009 \
+  hugo server --bind 0.0.0.0 -p 1313
