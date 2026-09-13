1b548b147c226f3c2ec85b0ea46d8ca4d0ec2dda
Marcin Witowski <mars3n@protonmail.com>
2026-09-13 13:37:36 +0200

    update script

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,3 @@
 docker run --rm -p 1313:1313 -v ${PWD}:/src \
-  docker.io/hugomods/hugo@sha256:cf1a2009a1edf807d0880af344c4dd13b40b308c340501128fb54e5aab17b009 \
+  docker.io/hugomods/hugo:0.165.0 \
   hugo server --bind 0.0.0.0 -p 1313
